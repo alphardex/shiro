f18b1aaf807b6e3ea5a49daf03a5eae031f6bb36
alphardex <2582347430@qq.com>
2019-10-04 22:56:14 +0800

    init

---
+++ src/components/Card.vue
@@ -0,0 +1,53 @@
+<template>
+  <figure class="imghvr-shutter-in-out-diag-2">
+    <img :src="thumbnailPath" :alt="title" />
+    <figcaption>
+      <h3>{{ title }}</h3>
+      <p>{{ description }}</p>
+    </figcaption>
+    <a :href="link" target="_blank"></a>
+  </figure>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Card extends Vue {
+  @Prop() private title!: string;
+  @Prop() private thumbnail!: string;
+  @Prop() private description!: string;
+  get link() {
+    return `/gallery/${this.title}/dist/`;
+  }
+  get thumbnailPath() {
+    return `/thumbnails/${this.thumbnail}`;
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+figure {
+  background: hsl(187, 40%, 25%);
+  box-shadow: 0 0 10px #263758;
+  border-radius: 0.5em;
+  padding: 0.8em;
+  transition: all 0.5s ease;
+
+  &:hover {
+    box-shadow: 0 0 40px #263758;
+  }
+
+  img {
+    max-width: 100%;
+  }
+
+  figcaption {
+    h3 {
+      font-size: 1em;
+      line-height: 1.5em;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,99 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h1 class="intro">Skadi</h1>
+    <div class="gallery">
+      <Card
+        v-for="art in arts"
+        :title="art.title"
+        :thumbnail="art.thumbnail"
+        :description="art.description"
+        :key="art.title"
+      ></Card>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Component, Vue, Provide } from "vue-property-decorator";
+import Card from "@/components/Card.vue";
+
+interface Art {
+  title: string;
+  thumbnail: string;
+  description: string;
+}
 
 @Component({
   components: {
-    HelloWorld
+    Card
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  @Provide() arts: Art[] = [
+    {
+      title: "abbr-expansion",
+      thumbnail: "abbr-expansion.png",
+      description: "缩写的展开效果"
+    },
+    {
+      title: "apple-watch-breeze",
+      thumbnail: "apple-watch-breeze.png",
+      description: "苹果手表上加载时的呼吸动画"
+    },
+    {
+      title: "gleaming-heading",
+      thumbnail: "gleaming-heading.png",
+      description: "隐约闪烁的标题"
+    },
+    {
+      title: "gleaming-loading",
+      thumbnail: "gleaming-loading.png",
+      description: "隐约闪烁的加载"
+    },
+    {
+      title: "gradient-background",
+      thumbnail: "gradient-background.png",
+      description: "动态的渐变背景"
+    },
+    {
+      title: "login-button",
+      thumbnail: "login-button.png",
+      description: "附带交互效果的登陆按钮"
+    },
+    {
+      title: "particle-burst",
+      thumbnail: "particle-burst.png",
+      description: "微粒向四周爆炸的效果"
+    },
+    {
+      title: "rgb-loading",
+      thumbnail: "rgb-loading.png",
+      description: "红绿蓝三色的加载环"
+    },
+    {
+      title: "snow-scratch",
+      thumbnail: "snow-scratch.png",
+      description: "刮雪效果，雪会再度凝结"
+    }
+  ];
+}
 </script>
+
+<style lang="scss">
+@import url(https://fonts.googleapis.com/css?family=Lobster);
+
+.intro {
+  color: hsl(187, 61%, 18%);
+  text-align: center;
+  font-size: 4em;
+  font-family: "Lobster";
+}
+
+.gallery {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(30ch, 1fr));
+  grid-gap: 2rem;
+
+  padding-top: 2.5rem;
+}
+</style>
